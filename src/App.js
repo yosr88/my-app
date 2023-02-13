daf2af6cd40806e969a21752a59f02530705b1d7
Yosr88 <Yosr.TRABELSI@ecole-hexagone.com>
2023-02-13 19:18:53 +0100

    friend-requests

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React, { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
 
 function FriendRequestTest({ request, onAccept, onDecline }) {
   return (
-    <div className="friend-request">
-      <img src={request.picture.data.url} alt={request.name} width={50} height={50} />
-      <p>{request.name}</p>
-      <button className="btn btn-success" style={{ marginRight: '10px' }} onClick={() => onAccept(request)}>Accept</button>
-      <button class="btn btn-danger" onClick={() => onDecline(request)}>Decline</button>
+    <div className="friend-request d-flex align-items-center">
+      <img src={request.picture.data.url} alt={request.name} className="friend-request-picture mr-3 rounded-circle" />
+      <div className="friend-request-info">
+        <p className="friend-request-name">{request.name}</p>
+        <div className="friend-request-buttons d-flex justify-content-between">
+          <button className="btn btn-warning mx-2" onClick={() => onAccept(request)}>Confirmer</button>
+          <button class="btn btn-light" onClick={() => onDecline(request)} 
+  style={{
+    backgroundColor: "transparent",
+    border: "1px solid violet",
+    color: "violet",
+    transition: "all 0.3s",
+    '&:hover': {
+      backgroundColor: "violet",
+      color: "white"
+    }
+  }}
+>
+  Supprimer l'invitation
+</button>
+        </div>
+      </div>
     </div>
   );
 }
+
 
 function App() {
   const [friendRequestsData, setFriendRequestsData] = useState([
@@ -20,6 +37,7 @@ function App() {
     { id: 3, name: "Thomas Hoareau", picture: {data: {url: "https://randomuser.me/api/portraits/med/men/1.jpg"}} },
     { id: 4, name: "Sinthy", picture: {data: {url: "https://randomuser.me/api/portraits/med/men/2.jpg"}} },
     { id: 5, name: "Mohcine Ergui", picture: {data: {url: "https://randomuser.me/api/portraits/med/men/3.jpg"}} },
+    
   ]);
 
   const handleAccept = (request) => {
@@ -52,6 +70,7 @@ function App() {
         <FriendRequestTest
           key={request.id}
           request={request}
+
           onAccept={handleAccept}
           onDecline={handleDecline}
         />
--- src/FriendRequest.js
+++ src/FriendRequest.js
@@ -4,7 +4,7 @@ function FriendRequest({ request }) {
     return (
       <div>
         <p>{request.name} has sent you a friend request</p>
-        <button type="button" class="btn btn-success">Accept</button>
+        <button type="button" class="btn btn-warning">Accept</button>
         <button type="button" class="btn btn-danger">Decline</button>
       </div>
     );
